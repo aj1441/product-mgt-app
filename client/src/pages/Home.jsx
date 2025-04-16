6ebab107bf06c81eed8687d04aa6c92759863083
aj1441 <167340937+aj1441@users.noreply.github.com>
2025-04-16 14:26:18 -0700

    tablet view

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,36 +1,42 @@
-import { useEffect, useState } from 'react'
-import Header from '../customCompoents/Header'
-import Category from '../customCompoents/Category'
-import FeedBackTiles from '../customCompoents/FeedBackTiles'
-import NoFeedback from '../customCompoents/NoFeedBack'
-import FeedbackBar from '../customCompoents/FeedbackBar'
+import { useEffect, useState } from "react";
+import Header from "../customCompoents/Header";
+import Category from "../customCompoents/Category";
+import FeedBackTiles from "../customCompoents/FeedBackTiles";
+import NoFeedback from "../customCompoents/NoFeedBack";
+import FeedbackBar from "../customCompoents/FeedbackBar";
 
 function Home() {
-  const [showSidebar, setShowSidebar] = useState(false)
-  const [feedbacks, setFeedbacks] = useState([])
-  const [category, setCategory] = useState('All')
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [feedbacks, setFeedbacks] = useState([]);
+  const [category, setCategory] = useState("All");
 
   useEffect(() => {
-    fetch('http://localhost:3000/api/feedback')
+    fetch("http://localhost:3000/api/feedback")
       .then((res) => res.json())
       .then((data) => setFeedbacks(data))
-      .catch((err) => console.error(err))
-  }, [])
+      .catch((err) => console.error(err));
+  }, []);
 
   const filteredFeedbacks =
-    category === 'All' ? feedbacks : feedbacks.filter(fb => fb.category === category)
+    category === "All"
+      ? feedbacks
+      : feedbacks.filter((fb) => fb.category === category);
 
   return (
     <div className="home">
-      <Header onToggleSidebar={setShowSidebar} />
-
-      {/* MOBILE CATEGORY MENU */}
-      {showSidebar && (
-        <>
-        <div className="grey-out" onClick={() => setShowSidebar(false)} />
-        <div className="mobile-sidebar-overlay">
+      <div className="top-bar">
+        <Header onToggleSidebar={setShowSidebar} />
+        <div className="category-inline">
           <Category selected={category} onSelect={setCategory} />
         </div>
+      </div>
+
+      {showSidebar && (
+        <>
+          <div className="grey-out" onClick={() => setShowSidebar(false)} />
+          <div className="mobile-sidebar-overlay">
+            <Category selected={category} onSelect={setCategory} />
+          </div>
         </>
       )}
       <FeedbackBar />
@@ -41,7 +47,7 @@ function Home() {
         }
       </main>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
